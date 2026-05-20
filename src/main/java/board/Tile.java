aa3109fef5d06b39ca032fc50426900b66091e91
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2026-05-20 16:43:56 -0500

    built tile class for usage in Board

---
+++ src/main/java/board/Tile.java
@@ -0,0 +1,33 @@
+package board;
+
+public class Tile {
+    private final TileType tileType;
+    private int numberToken;
+    private boolean hasRobber;
+
+    public Tile(TileType tileType) {
+        this.tileType = tileType;
+        this.numberToken = 0;
+        this.hasRobber = false;
+    }
+
+    public TileType getResourceType() {
+        return tileType;
+    }
+
+    public int getNumberToken() {
+        return numberToken;
+    }
+
+    public void setNumberToken(int numberToken) {
+        this.numberToken = numberToken;
+    }
+
+    public boolean hasRobber() {
+        return hasRobber;
+    }
+
+    public void setHasRobber(boolean hasRobber) {
+        this.hasRobber = hasRobber;
+    }
+}
