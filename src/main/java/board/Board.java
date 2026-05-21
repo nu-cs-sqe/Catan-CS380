45cdc97ee47ab7094e69b5c4eb90aaff35e1dfe0
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2026-05-21 17:37:57 -0500

    Implemented Board class so that it randomly generates a new board on running .create(), added getters as well. Updated Tile class to work with new structure, created test suite for Tile since it requires logic checks

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -1,20 +1,89 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Board {
+    private static final int[][] NEIGHBOR_OFFSETS = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, -1}, {-1, 1}
+    };
+
+    private static final int[][] POSITIONS = {
+        {-2, 0}, {-2, 1}, {-2, 2},
+        {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2},
+        {0, -2}, {0, -1}, {0, 0}, {0, 1}, {0, 2},
+        {1, -2}, {1, -1}, {1, 0}, {1, 1},
+        {2, -2}, {2, -1}, {2, 0}
+    };
+
+    private static final List<TileType> TILE_DISTRIBUTION = Arrays.asList(
+        TileType.FOREST, TileType.FOREST, TileType.FOREST, TileType.FOREST,
+        TileType.PASTURE, TileType.PASTURE, TileType.PASTURE, TileType.PASTURE,
+        TileType.FIELDS, TileType.FIELDS, TileType.FIELDS, TileType.FIELDS,
+        TileType.HILLS, TileType.HILLS, TileType.HILLS,
+        TileType.MOUNTAINS, TileType.MOUNTAINS, TileType.MOUNTAINS,
+        TileType.DESERT
+    );
+
+    private static final List<Integer> TOKEN_DISTRIBUTION = Arrays.asList(
+        2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12
+    );
+
     private final Shuffler shuffler;
-    private List<Tile> tiles;
+    private Map<String, Tile> tiles;
 
     public Board(Shuffler shuffler) {
         this.shuffler = shuffler;
     }
 
     public void create() {
-        // TODO: implement random board creation
+        List<TileType> types = new ArrayList<>(TILE_DISTRIBUTION);
+        shuffler.shuffle(types);
+
+        tiles = new HashMap<>();
+        for (int i = 0; i < POSITIONS.length; i++) {
+            int q = POSITIONS[i][0];
+            int r = POSITIONS[i][1];
+            tiles.put(key(q, r), new Tile(types.get(i), q, r));
+        }
+
+        List<Integer> tokens = new ArrayList<>(TOKEN_DISTRIBUTION);
+        shuffler.shuffle(tokens);
+
+        int tokenIndex = 0;
+        for (Tile tile : tiles.values()) {
+            if (tile.getTileType() == TileType.DESERT) {
+                tile.setHasRobber(true);
+            } else {
+                tile.setNumberToken(tokens.get(tokenIndex++));
+            }
+        }
     }
 
-    public List<Tile> getTiles() {
-        return tiles;
+    public Collection<Tile> getTiles() {
+        return tiles.values();
+    }
+
+    public Tile getTile(int q, int r) {
+        return tiles.get(key(q, r));
+    }
+
+    public List<Tile> getNeighbors(int q, int r) {
+        List<Tile> neighbors = new ArrayList<>();
+        for (int[] offset : NEIGHBOR_OFFSETS) {
+            Tile neighbor = tiles.get(key(q + offset[0], r + offset[1]));
+            if (neighbor != null) {
+                neighbors.add(neighbor);
+            }
+        }
+        return neighbors;
+    }
+
+    private static String key(int q, int r) {
+        return q + "," + r;
     }
 }
--- src/main/java/board/Tile.java
+++ src/main/java/board/Tile.java
@@ -1,33 +1,45 @@
 package board;
 
 public class Tile {
-    private final TileType tileType;
-    private int numberToken;
-    private boolean hasRobber;
+  private final TileType tileType;
+  private final int q;
+  private final int r;
+  private int numberToken;
+  private boolean hasRobber;
 
-    public Tile(TileType tileType) {
-        this.tileType = tileType;
-        this.numberToken = 0;
-        this.hasRobber = false;
-    }
+  public Tile(TileType tileType, int q, int r) {
+    this.tileType = tileType;
+    this.q = q;
+    this.r = r;
+    this.numberToken = 0;
+    this.hasRobber = false;
+  }
 
-    public TileType getTileType() {
-        return tileType;
-    }
+  public TileType getTileType() {
+    return tileType;
+  }
 
-    public int getNumberToken() {
-        return numberToken;
-    }
+  public int getQ() {
+    return q;
+  }
 
-    public void setNumberToken(int numberToken) {
-        this.numberToken = numberToken;
-    }
+  public int getR() {
+    return r;
+  }
 
-    public boolean hasRobber() {
-        return hasRobber;
-    }
+  public int getNumberToken() {
+    return numberToken;
+  }
 
-    public void setHasRobber(boolean hasRobber) {
-        this.hasRobber = hasRobber;
-    }
+  public void setNumberToken(int numberToken) {
+    this.numberToken = numberToken;
+  }
+
+  public boolean hasRobber() {
+    return hasRobber;
+  }
+
+  public void setHasRobber(boolean hasRobber) {
+    this.hasRobber = hasRobber;
+  }
 }
